2f4ce3d015d3e49c93f55ae4a3ac5210b08319cb
chrstianlagreca31 <chris06lagreca@gmail.com>
2025-02-28 09:23:53 +0100

    Add files via upload

---
+++ cicli3/main.js
@@ -0,0 +1,36 @@
+let scelta;
+
+/*while (true) {
+    scelta = Number(prompt("Seleziona una bevanda:\n1 - Acqua\n2 - Coca Cola\n3 - Birra"));
+
+    if (scelta === 1) {
+        console.log("E’ stata selezionata l’acqua");
+        break;
+    } else if (scelta === 2) {
+        console.log("E’ stata selezionata coca cola");
+        break;
+    } else if (scelta === 3) {
+        console.log("E’ stata selezionata birra");
+        break;
+    } else {
+        console.log("Scelta non valida, riprova.");
+    }
+}*/
+
+ 
+while (true) {
+    scelta = (prompt("Seleziona una bevanda:\n1 - Acqua\n2 - Coca Cola\n3 - Birra"));
+
+    if (scelta === 1) {
+        console.log("E’ stata selezionata l’acqua");
+        break;
+    } else if (scelta === 2) {
+        console.log("E’ stata selezionata coca cola");
+        break;
+    } else if (scelta === 3) {
+        console.log("E’ stata selezionata birra");
+        break;
+    } else {
+        console.log("Scelta non valida, riprova.");
+    }
+}
