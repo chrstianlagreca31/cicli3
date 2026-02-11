80a828fdd599ffc6009cc61dfc5be6b5b57b2cdd
chrstianlagreca31 <chris06lagreca@gmail.com>
2026-02-11 17:10:56 +0100

    Add files via upload

--- cicli3/main.js
+++ cicli3/main.js
@@ -1,36 +1,25 @@
 let scelta;
 
-/*while (true) {
-    scelta = Number(prompt("Seleziona una bevanda:\n1 - Acqua\n2 - Coca Cola\n3 - Birra"));
-
-    if (scelta === 1) {
-        console.log("E’ stata selezionata l’acqua");
-        break;
-    } else if (scelta === 2) {
-        console.log("E’ stata selezionata coca cola");
-        break;
-    } else if (scelta === 3) {
-        console.log("E’ stata selezionata birra");
-        break;
-    } else {
-        console.log("Scelta non valida, riprova.");
-    }
-}*/
-
- 
 while (true) {
-    scelta = (prompt("Seleziona una bevanda:\n1 - Acqua\n2 - Coca Cola\n3 - Birra"));
 
-    if (scelta === 1) {
-        console.log("E’ stata selezionata l’acqua");
+    scelta = prompt(
+        "Benvenuto al distributore!\n" +
+        "Seleziona una bevanda:\n" +
+        "1 - Acqua\n" +
+        "2 - Coca Cola\n" +
+        "3 - Birra"
+    );
+
+    if (scelta === "1") {
+        console.log("E' stata selezionata l'acqua");
         break;
-    } else if (scelta === 2) {
-        console.log("E’ stata selezionata coca cola");
+    } else if (scelta === "2") {
+        console.log("E' stata selezionata coca cola");
         break;
-    } else if (scelta === 3) {
-        console.log("E’ stata selezionata birra");
+    } else if (scelta === "3") {
+        console.log("E' stata selezionata birra");
         break;
     } else {
-        console.log("Scelta non valida, riprova.");
+        alert("Scelta non valida. Riprova.");
     }
 }
